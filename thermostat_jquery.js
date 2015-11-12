678c2f0732c1ded35e6982bab9418326fecd5500
Mattia46 <mattia.assogna@gmail.com>
2015-11-12 18:58:13 +0000

    completed all logic

--- thermostat_jquery.js
+++ thermostat_jquery.js
@@ -21,7 +21,30 @@ $(document).ready(function() {
       $("#switch").text("turn on"); }
     });
 
+    updateLocalWeather("london");
 
+    $("#location_form").submit(function(event){
+      updateLocalWeather($("#location").val());
+      event.preventDefault();
+    })
+
+
+    function _convertKtoC(kelvins){
+      return Math.round(kelvins - 273.15);
+    };
+
+    function showTempeature(resp) {
+      var apiTemp = _convertKtoC(resp.main.temp);
+      $("#weatherTest").text(apiTemp);
+    };
+
+    function updateLocalWeather(location){
+      var url = "http://api.openweathermap.org/data/2.5/weather";
+      var city = "q=" + location
+      var appID = "appid=5d80c46b555484695a8ba3f402702dd7";
+      var data = city +'&' + appID;
+      $.getJSON(url, data, showTempeature);
+    };
 
 
     function updateTemperature() {
